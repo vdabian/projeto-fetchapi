48fe086333c7142cd50dd29a4e32788a9fe349c1
Vinícius Dabian <vinicius_dabian@hotmail.com>
2021-04-13 21:40:25 -0300

    Questões 1 a 4 atualizado

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-function preencherDados(dados) {
-    document.getElementById('foto').innerHTML = "<img class='foto' src='"+dados.avatar_url+"'>";
-}
-
-
 function fetchAPI() {
     fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(dados => {console.log(dados)
-        console.log("<img class='foto' src='"+dados.message+"'></img>")
-        document.getElementById('dog').innerHTML = "<img class='foto' src='"+dados.message+"'></img>";
+        .then(response => response.json())
+        .then(dados => {
+            console.log(dados)
+            console.log("<img class='foto' src='" + dados.message + "'></img>")
+            document.getElementById('dog').innerHTML = "<img class='foto' src='" + dados.message + "'></img>";
         }
-    )
-    
+        )
+
 }
 
 
